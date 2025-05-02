2cc608ecbfdcc6c05c1bbfda51c2c4323db6f752
Dasun <dasuna39@gmail.com>
2025-05-02 21:15:31 +0530

    Initialize the cbc-backend project ✏️

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.use(bodyParser.json())
+
+
+mongoose.connect("mongodb+srv://admin:123@cluster0.uu2dhg2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("Connected to database"))
+    .catch(() => console.log("Failed to connect to database"))
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+})
